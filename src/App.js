c48fab3fcdb1095d98f37aa66d41b103b9d3136a
A5hReaper <amulama@student.unimelb.edu.au>
2021-09-05 13:10:23 +1000

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Navbar from './components/navbar/Navbar';
+import Login from './pages/Login';
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={Navbar} />
+          </Switch>
+      </Router>
     </div>
   );
 }
