fae6afb8f528a993f998b38668036b63baf7c133
Lucien Lu <lucien.lu@student.unimelb.edu.au>
2021-09-11 01:20:24 +1000

    Fixed env url use

--- src/SpringBootAdapter.js
+++ src/SpringBootAdapter.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import dotenv from 'dotenv'
+
 
 const fetch = (aUrl, headers) => {
-    dotenv.config();
     return axios({
         method: 'GET',
-        url: 'https://' + process.env.REACT_APP_BACKEND_URL + aUrl,
+        url: process.env.REACT_APP_BACKEND_URL + aUrl,
         headers: headers
     })
 };
@@ -18,7 +17,7 @@ const post = (aUrl, headers, body) => {
     aHeaders = headers !== null ? Object.assign(headers, aHeaders) : aHeaders;
     return axios({
         method: 'POST',
-        url: 'https://' + process.env.REACT_APP_BACKEND_URL + aUrl,
+        url: process.env.REACT_APP_BACKEND_URL + aUrl,
         headers: aHeaders,
         data: body
     })
