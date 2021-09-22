d8abdbef6527fab64598fb5919ddaf0567a07b8d
A5hReaper <amulama@student.unimelb.edu.au>
2021-09-22 14:58:19 +1000

    Add updated component for a single contact

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,32 +1,46 @@
 import { isPropsEqual } from '@fullcalendar/common';
 import React, { useState } from 'react';
 import { BiFilter } from 'react-icons/bi';
-import ProfilePicture from "./ProfilePicture"
+import ProfilePicture from "./ProfilePic/ProfilePicture"
 import ContactDetails from "./ContactDetails"
-import { ListGroup } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
 import EditContact from './EditContact';
+import { FiMoreHorizontal } from 'react-icons/fi';
 
 
 
-const Contact = ({contact}) => {
+
+const Contact = ({contact, onDelete}) => {
 
 	const [modalShow, setModalShow] = React.useState(false);
 
 	return (
 		<>
-			<li className="contact-item" onClick={() => setModalShow(true)}>
-				<ProfilePicture
-					name={contact.name}
-					id={contact.contactID}
-				/>
-				<h4>{contact.name}</h4>
+			<li className="contact-item" >
+				<div className="contact-info" onClick={() => setModalShow(true)}>
+					<ProfilePicture
+						name={contact.name}
+						id={contact.contactID}
+						size="md"
+					/>
+					<h4>{contact.name}</h4>
+				</div>
 
-				<EditContact
+				<Dropdown className="contact-options">
+					<Dropdown.Toggle id="button-dropdown-body"  className="dropdown-button" >
+						<FiMoreHorizontal className="edit-contact-options" size={30}/>
+					</Dropdown.Toggle>
+
+					<Dropdown.Menu className="contact-options-dropdown" variant="dark">
+						<Dropdown.Item onClick={() => onDelete(contact)}>Delete</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
+
+				<ContactDetails
 					contact={contact}
 					show={modalShow}
 					onHide={() => setModalShow(false)}
 				/>
-
 			</li>
 		</>
 	)
