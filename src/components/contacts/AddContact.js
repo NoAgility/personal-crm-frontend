9a221a29a4c9ea785917743e8dc58606d9194606
A5hReaper <amulama@student.unimelb.edu.au>
2021-09-22 14:56:05 +1000

    Fix  implementation for adding contacts

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,107 +1,99 @@
-import { isPropsEqual } from '@fullcalendar/common';
 import React, { useState } from 'react';
-import { BiChevronLeft } from 'react-icons/bi';
-import ProfilePicture from "./ProfilePicture"
+import { MdClose } from 'react-icons/md';
+import ProfilePicture from "./ProfilePic/ProfilePicture"
+import SearchBar from "../searchbar/SearchBar"
 import { Modal } from 'react-bootstrap';
 import  './ContactDetails.css'
+import  './AddContact.css'
+
 import { useHistory } from "react-router-dom";
+import ContactController from './ContactController.js'
 
-const AddContact = (props) => {
-	const [username, setUsername] = useState("");
-	const [dob, setDOB] = useState("");
-	const [day, setDay] = useState("");
-	const [month, setMonth] = useState("");
-	const [year, setYear] = useState("");
-
-	const history = useHistory();
+const AddContact = ({show, onHide, onAdd}) => {
+	const [usernameSearch, setUsernameSearch] = useState("");
+    const [queryFound, setQueryFound] = useState(false);
+    const [hasSearched, setHasSearched] = useState(false);
+    const [isAdded, setAddComplete] = useState(false);
+	const [result, setResult] = useState({});
 
 	const handleClose = (e) => {
 		e.preventDefault();
-		history.goBack();
+		setUsernameSearch("");
+		setResult({});
+		setQueryFound(false);
+		setAddComplete(false);
+		onHide(false);
+	}
+
+	const handleAdd = (e) => {
+		setAddComplete(true);
+		onAdd(result);
+	}
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		// search for username
+		const contact = await ContactController.fetchUserByUsername(usernameSearch)
+		if (contact) {
+			setQueryFound(true);
+			setResult(contact);
+		}
+		setHasSearched(true);
+	}
+
+	const queryResult = (name, username, id) => {
+			return (<div className="contact-search-result">
+				<ProfilePicture
+					name={name}
+					id={id}
+					size="md"
+				/>
+				<div className="column">
+					<h4>{name}</h4>
+					<p>@{username}</p>
+				</div>
+
+				<button
+					className={`add-contact-btn ${isAdded ? 'add-complete' : ''}`}
+					onClick={handleAdd}
+					disabled={isAdded}
+					>{isAdded ? 'Added' : 'Add'}
+				</button>
+			</div>)
+		}
+
+	const noResult = () => {
+		if (hasSearched) return <h4>Not found</h4>
 	}
 
 	return (
-
 		<>
 		<Modal
-			{...props}
+			show={show}
+			onHide={onHide}
 			size="md"
 			aria-labelledby="contained-modal-title-vcenter"
 			centered>
-			<Modal.Body className="contact-details">
-				<div className="contact-details-left">
-					<ProfilePicture
-						name={username}
-						id={2}
-					/>
-				</div>
-				<div className="contact-details-right">
-					<form>
-						<div className="username-selector">
-							<input
-								required
-								type="text"
-								name="username"
-								id="username"
-								value={username}
-								placeholder="Username"
-								className="contact-input"
-								onChange={event => {setUsername(event.target.value)}}>
-							</input>
-						</div>
-							<div className="dob-selector">
-							<input
-								required
-								min="1"
-								type="number"
-								name="day"
-								id="day"
-								value={day}
-								placeholder="Day"
-								className="contact-input"
-								onChange={event => {setDay(event.target.value)}}>
-							</input>
-							<select required name="month" onChange={setMonth}>
-								<option selected disabled hidden>Month</option>
-								<option value="1">January</option>
-								<option value="2">February</option>
-								<option value="3">March</option>
-								<option value="4">April</option>
-								<option value="5">May</option>
-								<option value="6">June</option>
-								<option value="7">July</option>
-								<option value="8">August</option>
-								<option value="9">September</option>
-								<option value="10">October</option>
-								<option value="11">November</option>
-								<option value="12">December</option>
-							</select>
-							<input
-								required
-								min={((new Date().getFullYear()) - 120).toString()}
-								max={(new Date().getFullYear()).toString()}
-								type="number"
-								name="year"
-								id="year"
-								value={year}
-								placeholder="Year"
-								className="contact-input"
-								onChange={event => {setYear(event.target.value)}}>
-							</input>
-						</div>
-						<div classname="add-contact-btns">
-							<button className="cancel-add-contact-btn" onClick={handleClose}>Cancel</button>
-							<button className="add-contact-btn" type="submit">Add</button>
-						</div>
+			<div className="close-add-contact">
+				<MdClose className="edit-contact-options" onClick={handleClose} size={30}/>
+			</div>
+			<Modal.Body className="add-contact">
+				<h1>Search for a Contact</h1>
 
-					</form>
+				<SearchBar
+					name="username"
+					colorMode="light"
+					width="md"
+					onSubmit={onSubmit}
+					placeholder="Username"
+					value={usernameSearch}
+					onChange={event => {setUsernameSearch(event.target.value)}}
+				/>
 
-				</div>
+				{queryFound ? queryResult(result.name, result.username, result.contactID) : noResult}
 
 			</Modal.Body>
 		</Modal>
-
-
 		</>
 	)
 }
