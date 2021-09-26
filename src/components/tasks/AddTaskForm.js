9b6516dd0a9be67782d78621cdbd63e7db58e81a
A5hReaper <amulama@student.unimelb.edu.au>
2021-09-26 13:54:59 +1000

    Fix Task CSS

--- src/components/tasks/AddTaskForm.js
+++ src/components/tasks/AddTaskForm.js
@@ -12,29 +12,29 @@ const AddMeetingForm = (props) => {
     }
     return (
         <React.Fragment>
-        <div className="form-container">
+        <div className="task-form-container">
             <form>
-                <div className="input">
-                    <input type="text" className="form-input" value={name} placeholder="Task Name" onChange={event => setName(event.target.value)}/>
-                    
+                <div className="task-input">
+                    <input type="text" className="task-form-input" value={name} placeholder="Task Name" onChange={event => setName(event.target.value)}/>
+
                 </div>
-                <div className="input">
-                    <input type="text" className="form-input" value={notes} placeholder="Notes" onChange={event => setNotes(event.target.value)}/>
+                <div className="task-input">
+                    <input type="text" className="task-form-input" value={notes} placeholder="Notes" onChange={event => setNotes(event.target.value)}/>
                 </div>
-                
+
                 <div>
-                    <label className="form-label">Deadline</label>
-                    <input className="form-time-input" type="time" value={time} onChange={event => setTime(event.target.value)}/>
-                    <input className="form-date-input" type="date" value={date} onChange={event => setDate(event.target.value)}/>
+                    <label className="task-form-label">Deadline</label>
+                    <input className="task-form-time-input" type="time" value={time} onChange={event => setTime(event.target.value)}/>
+                    <input className="task-form-date-input" type="date" value={date} onChange={event => setDate(event.target.value)}/>
                 </div>
             </form>
         </div>
-        <div className="buttons">
-        <button className="submit" type="submit" onClick={props.submit}>Add</button>
-        <button className="cancel" type="cancel" onClick={props.cancel}>Cancel</button>
+        <div className="task-buttons">
+        <button className="task-submit" type="submit" onClick={props.submit}>Add</button>
+        <button className="task-cancel" type="cancel" onClick={props.cancel}>Cancel</button>
         </div>
         </React.Fragment>
-    ) 
+    )
 }
 
 export default AddMeetingForm;
