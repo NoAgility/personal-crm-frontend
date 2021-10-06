e2d3be580f64fe4ff82c126fcaa1b40daed41ff3
A5hReaper <amulama@student.unimelb.edu.au>
2021-10-06 23:56:15 +1100

    Add Meeting controller to correspond to backend API

---
+++ src/components/meetings/MeetingController.js
@@ -0,0 +1,196 @@
+import SpringBootAdapterWrapper from '../../util/SpringBootAdapterWrapper';
+
+/**
+ * Controller singleton providing an interface to the backend API
+ */
+const MeetingController = {
+    /**
+     * Retrieve meetings for a user
+     * @returns a list of objects representing meetings
+     */
+    fetchMeetings: async () => {
+        try {
+            const data = await SpringBootAdapterWrapper.get('/meeting/getAccountMeetings')
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+            return data;
+        } catch (err) {
+            alert("Failed to fetch meetings");
+        }
+    },
+
+	/**
+     * Retrieve a meeting by ID
+     * @returns a list of objects representing meetings
+     */
+	 fetchMeetingByID: async (id) => {
+        try {
+            const data = await SpringBootAdapterWrapper.get(`/meeting/getMeetingByID?meetingID=${id}`)
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+            return data;
+        } catch (err) {
+            alert("Failed to fetch meetings");
+        }
+    },
+
+    /**
+     * Adds a meeting for a user
+     * @param {*} meeting The meeting to be added
+     */
+    addMeeting: async (meeting) => {
+        try {
+            const data = {
+                "accountIDs": meeting.contactIDs.length !== 0 ? meeting.contactIDs : [],
+                "meetingName": meeting.meetingName,
+                "meetingDescription": meeting.meetingDescription,
+                "meetingStart": meeting.meetingStart,
+                "meetingEnd": meeting.meetingEnd,
+                // "meetingMinutes": meeting.meetingMinutes,
+                // ...(meeting.meetingDeadline !== null) && {"deadline": meeting.meetingDeadline},
+                // ...(meeting.meetingPriority !== null && meeting.meetingPriority.length > 0) && {"priority": parseInt(meeting.meetingPriority)},
+            }
+            const res = await SpringBootAdapterWrapper.post('/meeting/createMeeting', "", data)
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+        } catch (err) {
+            alert("Failed to create meeting");
+        }
+    },
+
+    /**
+     * Deletes a meeting
+     * @param {*} meeting The meeting to be deleted
+     */
+    deleteMeeting: async (meeting) => {
+        try {
+            const data = { "meetingID" : meeting.meetingID };
+            await SpringBootAdapterWrapper.post('/meeting/deleteMeeting', "",  data)
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+        } catch (err) {
+            alert("Failed to delete meeting");
+        }
+    },
+
+	/**
+     * Updates a meeting
+     * @param {*} meeting The meeting to be updated
+     */
+	 updateMeeting: async (meeting) => {
+        try {
+            const data = {
+                "accountIDs": meeting.contactIDs.length !== 0 ? meeting.contactIDs : [],
+                "meetingName": meeting.meetingName,
+                "meetingDescription": meeting.meetingDescription,
+                "meetingStart": meeting.meetingStart,
+                "meetingEnd": meeting.meetingEnd,
+                // "meetingMinutes": meeting.meetingMinutes,
+                // ...(meeting.meetingDeadline !== null) && {"deadline": meeting.meetingDeadline},
+                // ...(meeting.meetingPriority !== null && meeting.meetingPriority.length > 0) && {"priority": parseInt(meeting.meetingPriority)},
+            }
+            await SpringBootAdapterWrapper.post('/meeting/editMeeting', "",  data)
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+        } catch (err) {
+            alert("Failed to edit meeting");
+        }
+    },
+
+
+	/**
+     * Add meeting minutes to a meeting
+     * @param {*} meeting The meeting to receive minutes
+     * @param {*} minutes The meeting minutes to be added
+     */
+	 addMinutes: async (meeting, minuteText) => {
+        try {
+            const data = {
+				"meetingID" : meeting.meetingID,
+				"minuteText" : minuteText
+			};
+            await SpringBootAdapterWrapper.post('/meeting/createMinute', "",  data)
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+        } catch (err) {
+            alert("Failed to create meeting minutes");
+        }
+    },
+
+	/**
+     * Delete meeting minutes for a meeting
+     * @param {*} meeting The meeting to delete minutes from
+     * @param {*} minuteID The meeting minutes ID to be deleted
+     */
+	 deleteMinutes: async (meeting, minuteID) => {
+        try {
+            const data = {
+				"meetingID" : meeting.meetingID,
+				"minuteID" : minuteID
+			};
+            await SpringBootAdapterWrapper.post('/meeting/deleteMinute', "",  data)
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+        } catch (err) {
+            alert("Failed to delete meeting minutes");
+        }
+    },
+
+	/**
+     * Edit meeting minutes for a meeting
+     * @param {*} meeting The meeting to edit minutes from
+     * @param {*} minuteID The meeting minutes ID to be edited
+     * @param {*} minuteText The updated meeting mintues
+     */
+	 editMinutes: async (meeting, minuteID, minuteText) => {
+        try {
+            const data = {
+				"meetingID" : meeting.meetingID,
+				"minuteID" : minuteID,
+				"minuteText" : minuteText
+			};
+            await SpringBootAdapterWrapper.post('/meeting/editMinute', "",  data)
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+        } catch (err) {
+            alert("Failed to edit meeting minutes");
+        }
+    },
+
+    /**
+     * Accepts an invitation to a meeting
+     * @param {*} meetingID The meeting to be updated
+     */
+	 acceptMeetingInvite: async (meetingID) => {
+        try {
+            const data = {
+                "meetingID": meetingID,
+            }
+            await SpringBootAdapterWrapper.post('/meeting/acceptMeeting', "",  data)
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+        } catch (err) {
+            alert("Failed to accept meeting");
+        }
+    },
+
+	/**
+     * Accepts an invitation to a meeting
+     * @param {*} meetingID The meeting to be updated
+     */
+	 declineMeetingInvite: async (meetingID) => {
+        try {
+            const data = {
+                "meetingID": meetingID,
+            }
+            await SpringBootAdapterWrapper.post('/meeting/declineMeeting', "",  data)
+				.then(res => { return res.data; } )
+                .catch(err => { throw err; });
+        } catch (err) {
+            alert("Failed to decline meeting");
+        }
+    }
+
+}
+
+export default MeetingController;
