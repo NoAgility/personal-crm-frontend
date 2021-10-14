2edc0d5740166fb5b831179ef717280398ca1240
Lucien Lu <lucien.lu@student.unimelb.edu.au>
2021-10-15 01:31:00 +1100

    Integrated public holidays and tasks

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,28 +1,37 @@
 import React from "react";
-import AddEventMenu from "./AddEventMenu";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import EventController from './EventController';
+import {useEffect, useState, useRef} from 'react';
 import "./Calendar.css";
 export default function Calendar() {
 
-  const events = [{
-    title: "today's event",
-    date: new Date()
-  }] //EventController.fetchEvents();
+	const [publicHolidays, setPublicHolidays] = useState([]);
+	const [tasks, setTasks] = useState([]);
+	const [height, setHeight] = useState("100%");
+	const ref = useRef(false);
+	useEffect(() => {
+		EventController.fetchPublicHolidays().then(res => setPublicHolidays(res));
+		EventController.fetchTasks().then(res => setTasks(res));
+		
+	}, []);
 
-  return ( <React.Fragment><div className = "calendar" >
-    <h2>This page is currently under maintenance and features are not working</h2>
-    <FullCalendar
-    plugins = {
-      [dayGridPlugin]
-    }
-    events = {
-      events
-    }
-    height = "85vh"/>
-    
-    </div>
-    <AddEventMenu/>
-    </React.Fragment>
-  );
+	return (<React.Fragment>
+		<div className = "calendar" >
+			<FullCalendar plugins = {[dayGridPlugin]}
+				events = {publicHolidays.concat(tasks)}
+				eventTimeFormat = {{
+					hour: '2-digit',
+					minute: '2-digit'
+				}}
+				initialView='dayGridMonth'
+				selectable={true}
+				dayMaxEvents={true}
+				handleWindowResize={true}
+				height={height}
+				windowResize={() => {setHeight("100%")}}/>
+				
+		</div>
+		</React.Fragment>
+	);
 }
--- src/components/calendar/EventController.js
+++ src/components/calendar/EventController.js
@@ -1,12 +1,29 @@
-import { fetch, post } from "../../util/SpringBootAdapter";
-
+import SpringBootAdapterWrapper from "../../util/SpringBootAdapterWrapper";
+import TaskController from "../tasks/TaskController";
 const EventController = {
     
     /**
      * Method to fetch events
      */
-    fetchEvents: () => {
-        //get
+    fetchPublicHolidays: async () => {
+        try{
+			const res = await SpringBootAdapterWrapper.get("/calendar/public_holiday/get").then(
+				response => { return (response.data.map(event => ({title: event.name, date: event.date}))); }
+			);
+			return res;
+		} catch (err) {
+			console.log(err)
+		}
+    },
+    fetchTasks: async () => {
+        try{
+			const res = await TaskController.fetchTasks().then(
+				data => { return (data.filter((event) => event.taskDeadline !== null).map(event => ({title: event.taskName, date: event.taskDeadline}))); }
+			);
+			return res;
+		} catch (err) {
+			console.log(err)
+		}
     },
     /**
      * Method to create event
